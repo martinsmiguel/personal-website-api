5d834baa9b6cc198b1496a06ab6c2aff278b67f8
martinsmiguel <miguelrjmartins9@gmail.com>
2020-03-20 18:12:44 -0400

    POST and GET to the projects Collection

--- functions/index.js
+++ functions/index.js
@@ -17,8 +17,18 @@ api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: false }));
 
 
+exports.api = functions.https.onRequest(api);
+
+//== == == == == == == == == == == == == == ==
+//Define 
+
+//Define Collection profiles
 const profilesCollection = 'profiles';
 
+//Define Collection projects
+const projectsCollection = 'projects';
+
+//Define class Profile
 class Profile {
     constructor(name, birth, email, password, office, description, tags, image, link) {
         this.name = name,
@@ -32,6 +42,25 @@ class Profile {
         this.link = link
     }
 }
+
+//Define class Project
+class Project {
+    constructor( type, date, title, description, author, collaborators, tags, image, link, status) {
+        this.type = type,
+        this.date = date,
+        this.title = title,
+        this.description = description,
+        this.author = author,
+        this.collaborators = collaborators,
+        this.tags = tags,
+        this.image = image,
+        this.link = link
+        this.status = status
+    }
+}
+
+//== == == == == == == == == == == == == == ==
+//ADD
 
 // Add new profile
 app.post('/profiles', async (req, res) => {
@@ -50,10 +79,10 @@ app.post('/profiles', async (req, res) => {
 
         const user = JSON.parse(JSON.stringify(profile));
 
-        const ref = await db.collection('profiles').add(user);
+        const refProfile = await db.collection('profiles').add(user);
 
         res.json({
-            id: ref.id,
+            id: refProfile.id,
             user
         });
         res.status(201).send(user);
@@ -63,6 +92,39 @@ app.post('/profiles', async (req, res) => {
     }        
 })
 
+// Add new project
+app.post('/projects', async (req, res) => {
+    try {
+        const project = new Project();
+
+        project.type = req.body['type'];
+        project.date = req.body['date'];
+        project.title = req.body['title'];
+        project.description = req.body['description'];
+        project.author = req.body['author'];
+        project.collaborators = req.body['collaborators'];
+        project.tags = req.body['tags'];
+        project.image = req.body['image'];
+        project.link = req.body['link'];
+        project.status = req.body['status'];
+
+        const prjt = JSON.parse(JSON.stringify(project));
+
+        const refProject = await db.collection('projects').add(prjt);
+        res.json({
+            id: refProject.id,
+            prjt
+        });
+        res.status(201).send(prjt);
+
+    } catch (error) {
+        res.status(400).send(prjt)
+    }        
+})
+
+//== == == == == == == == == == == == == == == 
+//View
+
 // View all profiles
 app.get('/profiles', (req, res) => {
     firebaseHelper.firestore
@@ -71,4 +133,27 @@ app.get('/profiles', (req, res) => {
         .catch(error => res.status(400).send(`Cannot get profiles: ${error}`));
 })
 
-exports.api = functions.https.onRequest(api);
+// View a profile
+app.get('/profiles/:profileId', (req, res) => {
+    firebaseHelper.firestore
+        .getDocument(db, profilesCollection, req.params.profileId)
+        .then(doc => res.status(200).send(doc))
+        .catch(error => res.status(400).send(`Cannot get profile: ${error}`));
+})
+
+
+// View all projects
+app.get('/projects', (req, res) => {
+    firebaseHelper.firestore
+        .backup(db, projectsCollection)
+        .then(data => res.status(200).send(data))
+        .catch(error => res.status(400).send(`Cannot get projects: ${error}`));
+})
+
+// View a project
+app.get('/projects/:projectId', (req, res) => {
+    firebaseHelper.firestore
+        .getDocument(db, projectsCollection, req.params.projectId)
+        .then(doc => res.status(200).send(doc))
+        .catch(error => res.status(400).send(`Cannot get profile: ${error}`));
+})
